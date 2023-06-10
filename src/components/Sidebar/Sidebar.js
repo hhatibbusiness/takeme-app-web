c72eef0562ef176e5b8eece40504423630689c6c
AhmedGomaaaAli <engahmedgomaa1997@gmail.com>
2023-06-11 02:01:44 +0300

    adding some features to the sidebar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Sidebar.scss';
 import {connect} from "react-redux";
+import {Link} from "react-router-dom";
 
 const Sidebar = ({assets, sidebar, setSidebar}) => {
     return (
@@ -8,6 +9,30 @@ const Sidebar = ({assets, sidebar, setSidebar}) => {
             <div className="Sidebar__container">
                 <div className="Sidebar__logo">
                     <img src={assets.logoPath} />
+                </div>
+                <div className="Sidebar__links">
+                    <Link to={'#'} className="Sidebar__link">
+                        <i className="fa-solid fa-circle-exclamation"></i>
+                        <p>من نحن؟</p>
+                    </Link>
+                    <div className="Sidebar__link">
+                        <i className="fa-solid fa-globe"></i>
+                        <p style={{direction: 'rtl'}}>اللغة-English</p>
+
+                    </div>
+                    <Link to={'#'} className="Sidebar__link">
+                        <i className="fa-regular fa-images"></i>
+                        <p>فلتر</p>
+                    </Link>
+                    <Link to={'#'} className="Sidebar__link">
+                        <i className="fa-solid fa-handshake"></i>
+                        <p>انضم كصاحب منتج أو خدمة</p>
+                    </Link>
+                    <Link to={'#'} className="Sidebar__link">
+                        <i className="fa-regular fa-copyright"></i>
+                        <p>العقد و شروط الاستخدام</p>
+                    </Link>
+                    <Link className={'Sidebar__link Sidebar__register'} to={'#'}>تسجيل الدخول</Link>
                 </div>
             </div>
         </div>
