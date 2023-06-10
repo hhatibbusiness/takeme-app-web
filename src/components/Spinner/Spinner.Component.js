0f8704dbed82a891a6420829fdf2f3a06a515e87
AhmedGomaaaAli <engahmedgomaa1997@gmail.com>
2023-06-10 20:46:29 +0300

    adding the spinner components

---
+++ src/components/Spinner/Spinner.Component.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Spinner.Styles.scss';
+
+const SpinnerComponent = () => {
+    return (
+        <div className={'Spinner'}>
+            <div className={'Spinner__spinner'}></div>
+        </div>
+    );
+};
+
+export default SpinnerComponent;
--- src/screens/Home/Body/Body.js
+++ src/screens/Home/Body/Body.js
@@ -1,17 +1,28 @@
 import React, {useEffect} from 'react';
 import {fetchCategories} from "../../../store/actions/categories.action";
 import {connect} from "react-redux";
+import SpinnerComponent from "../../../components/Spinner/Spinner.Component";
 
-const Body = ({ fetchCategories}) => {
+const Body = ({ fetchCategories, loadingCategories}) => {
 
     useEffect(() => {
         fetchCategories();
     }, []);
     return (
         <div className={'Body'}>
-            
+            {
+                loadingCategories ? (
+                    <SpinnerComponent />
+                ): (
+                    <div>Hello from this component</div>
+                )
+            }
         </div>
     );
 };
 
-export default connect(null, {fetchCategories}) (Body);
+const mapStateToProps = state => ({
+    loadingCategories: state.categories.loadingCategories
+})
+
+export default connect(mapStateToProps, {fetchCategories}) (Body);
