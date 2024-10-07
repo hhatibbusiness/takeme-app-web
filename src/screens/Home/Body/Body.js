e48e942596d5d26b05deced5c4363292f11a08c7
ahmed-gomaa2 <engahmedgomaa97@gmail.com>
2024-10-07 22:37:43 +0300

    cleaning the Body.js file

--- src/screens/Home/Body/Body.js
+++ src/screens/Home/Body/Body.js
@@ -1,19 +1,16 @@
-import React, {lazy, Suspense} from 'react';
+import React from 'react';
 import {fetchCategories} from "../../../store/actions/categories.action";
 import {connect} from "react-redux";
-import SpinnerComponent from "../../../components/Spinner/Spinner.Component";
 import BodyContainerComponent from "./BodyContainer/Body.Container.Component";
 import Sidebar from "../../../components/Sidebar/Sidebar";
 import Backdrop from "../../../components/Backdrop/Backdrop";
 import CategoryError from "../../../components/CategoryError/CategoryError";
 import './Body.scss'
-import {Outlet} from "react-router-dom";
 import {closePopup} from "../../../store/actions/ui.actions";
 
 const Body = ({ coverLoaded, setNavHeight, considerNav, setConsiderNav, fixedNav, setY, y, setFixedNav, topValue, setTopValue, setCoverLoaded, navShow, bodyContainerRef, setNavShow, setFiltersActive, filtersActive, navHeight, currentProduct, setCurrentProduct, setSidebar, sidebar, loadingCategories}) => {
     return (
             <div className={'Body'}>
-            {/*<div className={'Body'} style={{paddingTop: `${navHeight}px`}}>*/}
                 {
                     (loadingCategories) ? (
                         <div />
@@ -23,12 +20,6 @@ const Body = ({ coverLoaded, setNavHeight, considerNav, setConsiderNav, fixedNav
                         <BodyContainerComponent y={y} setY={setY} fixedNav={fixedNav} setFixedNav={setFixedNav} topValue={topValue} setTopValue={setTopValue} considerNav={considerNav}j bodyContainerRef={bodyContainerRef} setConsiderNav={setConsiderNav} setNavHeight={setNavHeight} navShow={navShow} setNavShow={setNavShow} filtersActive={filtersActive} setFiltersActive={setFiltersActive} navHeight={navHeight} coverLoaded={coverLoaded} setCoverLoaded={setCoverLoaded} currentProduct={currentProduct} setCurrentProduct={setCurrentProduct} />
                     </CategoryError>
                 }
-                {/*{*/}
-                {/*    (!coverLoaded) && <div className={'Body__hidden'}>*/}
-                {/*        <SpinnerComponent />*/}
-                {/*    </div>*/}
-                {/*}*/}
-                {/*<Outlet />*/}
             </div>
     );
 };
